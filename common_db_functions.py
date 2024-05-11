3d6b04661ecb7b5ad90d11a99a7d9b3a7d9a6e28
Rustam <rustam308@gmail.com>
2024-05-11 11:50:14 -0400

    Add player fetching, add season info to Game, minor changes

--- common_db_functions.py
+++ common_db_functions.py
@@ -1,4 +1,4 @@
-from models import Team, Player
+from models import Team, Player, Game
 from db_config import get_session
 
 def get_teams_from_db():
@@ -7,8 +7,16 @@ def get_teams_from_db():
     session.close()
     return teams
 
+
 def get_players_from_db():
     session = get_session()
     players = session.query(Player).all()
     session.close()
     return players
+
+
+def get_games_from_db():
+    session = get_session()
+    games = session.query(Game).all()
+    session.close()
+    return games
